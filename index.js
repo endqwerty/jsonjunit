d371d876e18dfeabf18bd2bdd8db992107a8aeee
Daniel <daniel.yang@amwell.com>
2020-08-02 18:26:30 -0700

    working version

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 #!/usr/bin/env node
-var program = require('commander'),
-    fs = require('fs'),
-    path = require('path'),
-    pkg = require('./package.json'),
-    jsonJunit = require('./lib/jsonjunit'),
-    fileName,
-    jsonFile,
-    junitFile;
+const program = require("commander");
+const fs = require("fs");
+const path = require("path");
+const pkg = require("./package.json");
+const jsonJunit = require("./lib/jsonjunit");
+
+let fileName;
+let jsonFile;
+let junitFile;
 
 program
-    .version(pkg.version)
-    .option('-ju, --json <json>', 'JSON report target path')
-    .option('-jx, --junit <junit>', 'JUnitXML report destination path')
-    //.option('-jt, --jsontype', 'Type of JSON report to convert')
-    .parse(process.argv);
+  .version(pkg.version)
+  .option("-ju, --json <json>", "JSON report target path")
+  .option("-jx, --junit <junit>", "JUnitXML report destination path")
+  .parse(process.argv);
 
 if (!program.json || !program.junit) {
-    console.log('You must specify both JSON and JUNIT path!');
-    process.exit(1);
+  console.log("You must specify both JSON and JUNIT path!");
+  process.exit(1);
 } else {
-    //create JUnitXML report dir if it doesn't exist
-    if (!fs.existsSync(program.junit)) {
-        fs.mkdirSync(program.junit);
-    }
+  // create JUnitXML report dir if it doesn't exist
+  if (!fs.existsSync(program.junit)) {
+    fs.mkdirSync(program.junit);
+  }
 
-    var jsonFiles = fs.readdirSync(program.json);
+  const jsonFiles = fs.readdirSync(program.json);
 
-    for(var i in jsonFiles) {
-        if (jsonFiles[i].indexOf('.json')>-1) {
-            jsonFile = path.join(program.json, jsonFiles[i]);
-            fileName = jsonFiles[i].slice(0, jsonFiles[i].indexOf('.json'));
-            junitFile = path.join(program.junit, fileName + ".xml");
-            jsonJunit.convertJson(jsonFile, junitFile);
-        }
+  for (const i in jsonFiles) {
+    if (jsonFiles[i].indexOf(".json") > -1) {
+      jsonFile = path.join(program.json, jsonFiles[i]);
+      fileName = jsonFiles[i].slice(0, jsonFiles[i].indexOf(".json"));
+      junitFile = path.join(program.junit, `${fileName}.xml`);
+      jsonJunit.convertJson(jsonFile, junitFile);
     }
+  }
 }
